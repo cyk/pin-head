5963a6134a165c22f4c14c09cf6e1394ef857acc
cyk <cytklassen@gmail.com>
2016-05-05 20:33:07 -0700

    Sentiment service, rePin when your face is JOY!

---
+++ src/client/app/shared/services/sentiment.service.ts
@@ -0,0 +1,22 @@
+import {Injectable} from 'angular2/core';
+import {Http, Response} from 'angular2/http';
+import {Observable} from 'rxjs/Observable';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/concatMap';
+
+@Injectable()
+export class SentimentService {
+
+  constructor(private http: Http) {}
+
+  get(stream$: Observable<{}>): Observable<{}> {
+    return stream$.concatMap((dataUri: string) => this.analyze(dataUri));
+  }
+
+  private analyze(image: string): Observable<string> {
+    let body = JSON.stringify({ image });
+    return this.http
+      .post('<%= SENTIMENT_API_URL %>/face-sentiments', body)
+      .map((response: Response) => response.json().likely_sentiment);
+  }
+}
--- src/client/app/shared/services/webcam.service.ts
+++ src/client/app/shared/services/webcam.service.ts
@@ -1,5 +1,7 @@
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/bindCallback';
+import 'rxjs/add/operator/bufferCount';
+import 'rxjs/add/operator/concatMap';
 import * as Webcam from 'webcamjs';
 
 export class WebcamService {
@@ -9,7 +11,15 @@ export class WebcamService {
   attach(elem: string | HTMLElement) {
     Webcam.attach(elem);
   }
+  snaps(stream$: Observable<{}>): Observable<{}> {
+    return stream$
+      .bufferCount(2, 1)
+      .concatMap(() => this.snap());
+  }
   snap(): Observable<string> {
-    return Observable.bindCallback(Webcam.snap.bind(Webcam), (dataUri: string) => dataUri)();
+    return Observable.bindCallback(
+      Webcam.snap.bind(Webcam),
+      (dataUri: string) => dataUri.replace('data:image/jpeg;base64,', '')
+    )();
   }
 }
